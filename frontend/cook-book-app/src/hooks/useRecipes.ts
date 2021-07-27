62acbb73998931de36ee93d666906e9a80b01606
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-07-27 21:17:52 +0200

    useRecipes.ts hook edit

--- frontend/cook-book-app/src/hooks/useRecipes.ts
+++ frontend/cook-book-app/src/hooks/useRecipes.ts
@@ -1,15 +1,27 @@
 import axios, { AxiosResponse } from "axios";
+import { useQuery } from "react-query";
 import { Recipe } from "../model/recipe";
+
 
 const api = axios.create();
 
 export default function useRecipes() {
-  const getRecipes = async () => {
-    const result = await api.get<Recipe, AxiosResponse<Recipe[]>>(
+  const fetchRecipes = async () => {
+    const result = await api.get(
       `https://randomuser.me/api/?results=25`
     );
     return result.data;
   };
 
-  return [getRecipes];
+
+  const searchRecipes = (searchTerm: string, state: any) => {
+    // let filteredData = data?.filter(d => d.Title.includes(searchTerm))
+
+    // if (filteredData) return filteredData;
+
+    // return {};
+    console.log(state);
+  }
+
+  return {fetchRecipes, searchRecipes};
 }
