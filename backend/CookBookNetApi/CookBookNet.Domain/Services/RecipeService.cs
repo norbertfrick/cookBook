e8604904dc352cade82eb1350dcbc7c21c12ae61
Norbert Frick <norbert.frick.dev@itvkurze.sk>
2021-09-12 22:29:35 +0200

    recipe repository and service added

---
+++ backend/CookBookNetApi/CookBookNet.Domain/Services/RecipeService.cs
@@ -0,0 +1,67 @@
+﻿using CookBookNet.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBookNet.Domain.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRepository<Recipe> repository;
+        private readonly IImageHandlerService imageHandler;
+
+        public RecipeService(IRepository<Recipe> repo, IImageHandlerService imageHandler)
+        {
+            this.repository = repo;
+            this.imageHandler = imageHandler;
+        }
+
+        public async Task<Recipe> AddRecipe(Recipe recipe)
+        {
+            var imagePath = imageHandler.SaveImage(recipe.Image.FileName, recipe.Image);
+            recipe.ImagePath = imagePath;
+            
+            return this.repository.Create(recipe);
+        }
+
+        public async Task<Recipe> DeleteRecipe(Guid id)
+        {
+            var recipe = this.repository.GetSingle(id);
+            this.repository.Delete(id);
+
+            return recipe;
+        }
+
+        public async Task<List<Recipe>> GetAll()
+        {
+            return this.repository.GetAll().ToList();
+        }
+
+        public async Task<Recipe> GetById(Guid id)
+        {
+            return this.repository.GetSingle(id);
+        }
+
+        public async Task<Recipe> GetByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateRecipe(Guid id, Recipe recipe)
+        {
+            var storedRecipe = this.repository.GetSingle(id);
+
+            if (recipe.ImagePath != storedRecipe.ImagePath)
+            {
+                //if image names differ, update images and update path to the image
+                recipe.ImagePath = this.imageHandler.SaveImage(recipe.Image.FileName, recipe.Image);
+            }
+
+            this.repository.Update(id, recipe);
+        }
+
+
+    }
+}
---
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Repositories/RecipeRepository.cs
@@ -0,0 +1,38 @@
+﻿using CookBookNet.Domain;
+using CookBookNet.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBookNet.Infrastructure.DA.Repositories
+{
+    public class RecipeRepository : IRepository<Recipe>
+    {
+        public async Task<Recipe> Create(Recipe entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Recipe>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Recipe> GetSingle(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Recipe> Update(Guid id, Recipe entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
