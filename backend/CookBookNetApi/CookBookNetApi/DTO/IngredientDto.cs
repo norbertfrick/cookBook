46f8918e3df24ce4430a4acf081d50f794504848
Norbert Frick <norbert.frick.dev@itvkurze.sk>
2021-09-12 22:29:19 +0200

    cookingStep and Ingredient changed to dtos

---
+++ backend/CookBookNetApi/CookBookNetApi/DTO/IngredientDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBookNetApi
+{
+    public class IngredientDto
+    {
+        public decimal Amount { get; set; }
+
+        public string Unit { get; set; }
+
+    }
+
+
+}
