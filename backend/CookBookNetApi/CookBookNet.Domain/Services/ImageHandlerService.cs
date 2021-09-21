2b89af908aa02b24388275a49ed9d8a4e4c662e2
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-09-21 15:02:14 +0200

    image handler service added

---
+++ backend/CookBookNetApi/CookBookNet.Domain/Services/ImageHandlerService.cs
@@ -0,0 +1,23 @@
+﻿using CookBookNet.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBookNet.Domain.Services
+{
+    public class ImageHandlerService : IImageHandlerService
+    {
+        public IFormFile GetImage(string imageName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveImage(string imageName, IFormFile image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
