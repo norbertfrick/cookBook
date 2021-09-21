852c682baa8945da6c705b747e6eabbde7c08a56
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-09-21 15:02:57 +0200

    loader changes

--- backend/CookBookNetApi/CookBookNet.Infrastructure/ModuleLoader.cs
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/ModuleLoader.cs
@@ -1,6 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CookBookNet.Domain;
+using CookBookNet.Infrastructure.Authentication.PasswordEncryption;
+using CookBookNet.Domain.Services;
+using CookBookNet.Infrastructure.Authentication;
+using CookBookNet.Infrastructure.DA.Repositories;
+using CookBookNet.Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Threading.Tasks;
 
 namespace CookBookNet.Infrastructure
 {
@@ -11,6 +19,35 @@ namespace CookBookNet.Infrastructure
             var domainModule = new DefaultDomainLoader();
 
             domainModule.Configure(configuration, services);
+
+            services.AddScoped<IPasswordEncryptionProvider, PasswordEncryptionProvider>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            services.AddScoped<IRepository<Recipe>, RecipeRepository>();
+            services.AddScoped<IRepository<User>, UserRepository>();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = (context) =>
+                    {
+                        var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+                        var userId = Guid.Parse(context.Principal.Identity.Name);
+                        var user = userService.GetById(userId);
+                        if (user == null)
+                        {
+                            // return unauthorized if user no longer exists
+                            context.Fail("Unauthorized");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
         }
     }
 }
--- backend/CookBookNetApi/CookBookNetApi/Startup.cs
+++ backend/CookBookNetApi/CookBookNetApi/Startup.cs
@@ -1,4 +1,6 @@
 using CookBookNet.Infrastructure;
+using CookBookNet.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,16 +22,14 @@ namespace CookBookNetApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //    .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
-
-            ModuleLoader.Load(this.Configuration, services);
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CookBookNetApi", Version = "v1" });
             });
+
+            ModuleLoader.Load(this.Configuration, services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
