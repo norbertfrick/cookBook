64b680651df6945c084ca2bcd0025d4ee830c4ab
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-09-21 15:01:56 +0200

    user service and repository

---
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using CookBookNet.Domain;
+using CookBookNet.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CookBookNet.Infrastructure.DA.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        public Task<User> Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetSingle(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(Guid id, User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
