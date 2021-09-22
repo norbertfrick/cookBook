4c2c9c188e4810f7e751cca3ef31378fdc97df91
Norbert Frick <norbert.frick.dev@itvkurze.sk>
2021-09-22 10:20:24 +0200

    Data contexts added

---
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Context/CookBookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CookBookNet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBookNet.Infrastructure.DA.Context
+{
+    public class CookBookDbContext : DbContext
+    {
+        public CookBookDbContext(DbContextOptions<CookBookDbContext> options)
+        : base(options)
+        
+        { }
+        public DbSet<Recipe> Customers { get; set; }
+        public DbSet<User> Invoices { get; set; }
+        public DbSet<Category> InvoiceItems { get; set; }
+    }
+}
---
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Context/CookBookMockDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CookBookNet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBookNet.Infrastructure.DA.Context
+{
+    public class CookBookMockDbContext : DbContext
+    {
+        public CookBookMockDbContext(DbContextOptions<CookBookDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Recipe> Customers { get; set; }
+        public DbSet<User> Invoices { get; set; }
+        public DbSet<Category> InvoiceItems { get; set; }
+    }
+}
