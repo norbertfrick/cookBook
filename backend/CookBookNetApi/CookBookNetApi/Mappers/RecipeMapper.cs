037d1d79b6184c311495c12da712466a20223254
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-09-29 19:13:44 +0200

    recipe mapper edited

--- backend/CookBookNetApi/CookBookNetApi/Mappers/RecipeMapper.cs
+++ backend/CookBookNetApi/CookBookNetApi/Mappers/RecipeMapper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace CookBookNetApi.Mappers
 {
@@ -11,12 +12,32 @@ namespace CookBookNetApi.Mappers
     {
         public Recipe MapFromDto(RecipeDto dto)
         {
-            throw new NotImplementedException();
+            return new Recipe
+            {
+                Id = dto.Id,
+                CategoryId = dto.CategoryId,
+                Description = dto.Description,
+                Image = dto.Image,
+                Ingredients = JsonSerializer.Serialize(dto.Ingredients),
+                Steps = JsonSerializer.Serialize(dto.CookingSteps),
+                Title = dto.Title,
+                UserId = dto.UserId
+            };
         }
 
         public RecipeDto MapToDto(Recipe entity)
         {
-            throw new NotImplementedException();
+            return new RecipeDto 
+            {
+                Id = entity.Id,
+                CategoryId = entity.CategoryId,
+                CookingSteps = JsonSerializer.Deserialize<List<CookingStepDto>>(entity.Steps),
+                Ingredients = JsonSerializer.Deserialize<List<IngredientDto>>(entity.Ingredients),
+                Title = entity.Title,
+                Description = entity.Description,
+                UserId = entity.UserId,
+                Image = entity.Image
+            };
         }
     }
 }
