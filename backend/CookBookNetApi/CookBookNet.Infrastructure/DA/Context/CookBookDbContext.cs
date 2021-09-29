98193ecedd3605530e5dfc8e8ee1436410c1a7bb
Norbert Frick <norbert.frick.external@hexagonsi.com>
2021-09-29 19:10:31 +0200

    Mock context removed and CookBookDbContext updated

--- backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Context/CookBookDbContext.cs
+++ backend/CookBookNetApi/CookBookNet.Infrastructure/DA/Context/CookBookDbContext.cs
@@ -1,16 +1,24 @@
 ﻿using CookBookNet.Domain;
+using CookBookNet.Infrastructure.Authentication.PasswordEncryption;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace CookBookNet.Infrastructure.DA.Context
 {
     public class CookBookDbContext : DbContext
     {
-        public CookBookDbContext(DbContextOptions<CookBookDbContext> options)
+        private readonly IPasswordEncryptionProvider passwordProvider;
+
+        public CookBookDbContext(DbContextOptions<CookBookDbContext> options, IPasswordEncryptionProvider passwordProvider)
         : base(options)
         
-        { }
-        public DbSet<Recipe> Customers { get; set; }
-        public DbSet<User> Invoices { get; set; }
-        public DbSet<Category> InvoiceItems { get; set; }
+        {
+           
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        
     }
 }
