25f9e86eb775dfe9f4c50280aeb2ae8099e536b9
Norbert Frick <norbertf@rcomputer.eu>
2022-08-05 17:49:08 +0200

    mock data edit

--- frontend/cook-book-app/src/mockData.ts
+++ frontend/cook-book-app/src/mockData.ts
@@ -6,16 +6,74 @@ export const mockData: Recipe[] =
             Id: "1",
             Title: "Skvela kolozvarska dobrota",
             Description: "Najlepsia kolozvarska kapusta tvojho zivota",
-            PictureUrl: "https://dobruchut.aktuality.sk/recept/foto/75441/1/kolozvarska-kapusta/",
+            PictureUrl: "https://s.aktuality.sk/img/L7I9E8fiRU_m_FwSyLdLgQ.jpg?st=DytaTCj951XfdqA4ujPShwaQFJjZ_NC04Ony0RWtPRA&ts=1614339081&e=0",
             User: "Norqo",
-            Tags: []
+            Tags: ["Popicovky"]
 
         },
         {
             Id: "2",
             Title: "Fakt chutna zemlovka premium",
             Description: "Taka zemlovka, ze ani neveris, ze nie je zo zemli",
-            PictureUrl: "https://dobruchut.aktuality.sk/recept/foto/69871/1/zemlovka-s-tvarohom-a-jablkami/",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
+            User: "Norqo",
+            Tags: []
+        },
+        {
+            Id: "3",
+            Title: "Priemerne chutna zemlovka",
+            Description: "Priemerne chutna zemlovka zo zemle",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
+            User: "Norqo",
+            Tags: []
+        },
+        {
+            Id: "4",
+            Title: "Skvela kolozvarska dobrota",
+            Description: "Najlepsia kolozvarska kapusta tvojho zivota",
+            PictureUrl: "https://s.aktuality.sk/img/L7I9E8fiRU_m_FwSyLdLgQ.jpg?st=DytaTCj951XfdqA4ujPShwaQFJjZ_NC04Ony0RWtPRA&ts=1614339081&e=0",
+            User: "Norqo",
+            Tags: ["Popicovky"]
+
+        },
+        {
+            Id: "5",
+            Title: "Fakt chutna zemlovka premium",
+            Description: "Taka zemlovka, ze ani neveris, ze nie je zo zemli",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
+            User: "Norqo",
+            Tags: []
+        },
+        {
+            Id: "6",
+            Title: "Priemerne chutna zemlovka",
+            Description: "Priemerne chutna zemlovka zo zemle",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
+            User: "Norqo",
+            Tags: []
+        },
+        {
+            Id: "7",
+            Title: "Skvela kolozvarska dobrota",
+            Description: "Najlepsia kolozvarska kapusta tvojho zivota",
+            PictureUrl: "https://s.aktuality.sk/img/L7I9E8fiRU_m_FwSyLdLgQ.jpg?st=DytaTCj951XfdqA4ujPShwaQFJjZ_NC04Ony0RWtPRA&ts=1614339081&e=0",
+            User: "Norqo",
+            Tags: ["Popicovky"]
+
+        },
+        {
+            Id: "8",
+            Title: "Fakt chutna zemlovka premium",
+            Description: "Taka zemlovka, ze ani neveris, ze nie je zo zemli",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
+            User: "Norqo",
+            Tags: []
+        },
+        {
+            Id: "9",
+            Title: "Priemerne chutna zemlovka",
+            Description: "Priemerne chutna zemlovka zo zemle",
+            PictureUrl: "https://s.aktuality.sk/img/EmBGYmR7QgC6FWINqN1Lag.jpg?st=50dHKotvePsnnUIofbKOrzzGKw4JxzT1O7d81Mt14EA&ts=1533282094&e=0",
             User: "Norqo",
             Tags: []
         }
