66904f5066fa785b33411db06f2817c2f70e0cb3
Norbert Frick <norbertf@rcomputer.eu>
2022-08-05 17:48:45 +0200

    file removal and renaming

--- frontend/cook-book-app/src/App.tsx
+++ frontend/cook-book-app/src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import Header from "./components/header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Main from "./features/main/main";
+import {SearchPage} from "./features/searchPage/searchPage";
+import RecipeDetailPage from "./features/recipeDetailPage/recipeDetailPage";
+import MainPage from "./features/mainPage/mainPage";
 
 function App() {
   
 
   return (
-    <div className="App">
+    <div className="App content-center">
       <Header></Header>
         <BrowserRouter>
           <Switch>
-            <Route path="/" exact component={Main}></Route>
+            <Route path="/" exact component={MainPage}></Route>
+            <Route path="/recipe/:id" exact component={RecipeDetailPage}></Route>
+            <Route path="/search" component={SearchPage}></Route>
           </Switch>
         </BrowserRouter>
     </div>
--- frontend/cook-book-app/src/features/mainPage/mainPage.tsx
+++ frontend/cook-book-app/src/features/mainPage/mainPage.tsx
@@ -1,45 +1,63 @@
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
 import SearchBar from "../../components/searchBar/searchBar";
 import useRecipes from "../../hooks/useRecipes";
 import { useHistory } from "react-router-dom";
 import { Recipe } from "../../model/recipe";
-import { mockData } from "../../mockData";
-import backgroundImage from "../../assets/pexels-valeria-boltneva-1860205.jpg"
 
-export default function Main() {
-  const { fetchRecipes, searchRecipes } = useRecipes();
+import backgroundImage from "../../assets/pexels-valeria-boltneva-1860205.jpg"
+import { ResultsPane } from "./resultsPane";
+import { SearchPageProps } from "../searchPage/searchPage";
+
+export default function MainPage() {
+  const [ mockRecipes ] = useRecipes();
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [searchResults, setSearchResults] = useState<Recipe[]>([]);
   const history = useHistory();
-  const data = mockData;
-  //const { data, status, isError, isLoading, isFetching } = useQuery("recipes", fetchRecipes);
 
-  useEffect(() => {
-    setRecipes(data);
-  }, [data]);
+useEffect(() => {
+  const getRec = async () =>{
+    let recipes = await mockRecipes();
+    setRecipes(recipes);
+  }
+  getRec();
+}, [])
 
   const recipeCardOnClick = (recipe: Recipe) => {
     history.push(`recipes/${recipe.Id}`);
   };
 
   const onSearch = (keyword: string) => {
+    let props: SearchPageProps = {
+      keyword: keyword
+    }
 
+      history.push(`search?searchTerm=${keyword}`, props);
   };
 
   const onSearchInputChange = (keyword: string) => {
+    if (keyword === '') {
+      setSearchResults([]);
+      return;
+    }
 
+    const results = searchRecipes(keyword);
+    setSearchResults(results);
   }
 
+  const searchRecipes = (keyword:string): Recipe[] => {
+    let results = recipes.filter(r => r.Title.toLowerCase().includes(keyword.toLowerCase()) || r.Description.toLowerCase().includes(keyword.toLowerCase()) || r.Tags.includes(keyword));
+
+    return results;
+  }
 
   return (
     <div style={{backgroundImage: `url(${backgroundImage})`}} className="p-5 bg-origin-padding relative w-screen h-screen bg-no-repeat bg-top bg-cover overflow-hidden gap-0 max-h-screen justify-items-center items-center">
-
         <div className="relative top-1/3 left-1/3 grid-cols-1 z-10 w-2/5 justify-self-center">
           <SearchBar
             search={onSearch}
             inputTextChange={onSearchInputChange}
           ></SearchBar>
+          <ResultsPane cardOnClick={recipeCardOnClick} searchResults={searchResults}></ResultsPane>
         </div>
     </div>
   );
