5c83d3e874d3e18d02762b54ca3615bd585f9795
Norbert Frick <norbertf@rcomputer.eu>
2022-08-18 09:29:26 +0200

    default picture added for result card

--- frontend/cook-book-app/src/features/searchPage/resultCard.tsx
+++ frontend/cook-book-app/src/features/searchPage/resultCard.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 import { Recipe } from "../../model/recipe";
+import defaultImage from "../../assets/food-placeholder.jpg"
 
 export interface ResultCardProps {
     recipe: Recipe;
@@ -11,7 +12,7 @@ export const ResultCard: FunctionComponent<ResultCardProps> = (props: ResultCard
 
     return (
             <Link to={`./recipe/${props.recipe.Id}`} className="flex-col col-span-2 lg:col-span-3 items-center bg-white rounded-lg m-3 border shadow-md lg:flex-row l:max-w-xl w-72 hover:bg-gray-100 hover:shadow-lg dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 cursor-pointer overflow-hidden">
-                <img className="object-cover w-full rounded-t-lg l:h-auto h-3/6 md:rounded-t-lg" src={props.recipe.PictureUrl} alt={props.recipe.Title} />
+                <img className="object-cover w-full rounded-t-lg l:h-auto h-3/6 md:rounded-t-lg" src={props.recipe.PictureUrl === "" ? defaultImage: props.recipe.PictureUrl} alt={props.recipe.Title} />
                 <div className="flex flex-col justify-between p-4 ">
                     <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white p-5">{props.recipe.Title}</h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 p-5 text-xs text-left">{props.recipe.Description}</p>
