330ef592029dd080fdbc81ebb688625504f5a923
Norbert Frick <norbertf@rcomputer.eu>
2024-01-13 00:00:00 +0100

    eQuantityUnit enum added

---
+++ backend/Cookbook/Cookbook.Domain/Enums/eQuantityUnit.cs
@@ -0,0 +1,15 @@
+namespace Cookbook.Domain.Enums
+{
+    public enum eQuantityUnit
+    {
+        None,
+        ml,
+        cl,
+        dl,
+        l,
+        mg,
+        g,
+        dg,
+        kg
+    }
+}
