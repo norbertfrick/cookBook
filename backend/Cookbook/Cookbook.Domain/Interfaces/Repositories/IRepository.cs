a12c2155eebaf11c9b9c7c0e2a0e1f3ad46b6592
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-01-15 15:12:15 +0100

    IRepository and IRecipeRepository created

---
+++ backend/Cookbook/Cookbook.Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Domain.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        T GetById(Guid id);
+
+        T Delete(Guid id);
+
+        T Update(Guid id, T newObject);
+
+        IEnumerable<T> GetAll();
+    }
+}
