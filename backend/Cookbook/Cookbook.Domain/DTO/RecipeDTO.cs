e07bb0ebf41a6d5f6256d7062daf6126a4500f64
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-01-15 15:15:23 +0100

    RecipeDTO added

---
+++ backend/Cookbook/Cookbook.Domain/DTO/RecipeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Domain.DTO
+{
+    public class RecipeDTO
+    {
+        public Guid Id { get; set; }
+
+        public string TitleImageUrl { get; set;}
+
+        public Guid OwnerId { get; set; }
+
+        public Guid DetailId { get; set; }
+    }
+}
