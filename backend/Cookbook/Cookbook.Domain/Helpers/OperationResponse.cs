994dbdff33bada03f0b93fa9240ed519c530f088
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-01-15 15:15:32 +0100

    OperationResponse added

---
+++ backend/Cookbook/Cookbook.Domain/Helpers/OperationResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Domain.Helpers
+{
+    public class OperationResponse
+    {
+        public OperationResponse(bool isSuccess, object data, string message)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+            Message = message;
+        }
+
+        public OperationResponse(bool isSuccess, object data): this(isSuccess, data, string.Empty)
+        {
+        }
+
+        public bool IsSuccess { get; set; }
+
+        public object Data { get; set; }
+
+        public string Message { get; set; }
+    }
+}
