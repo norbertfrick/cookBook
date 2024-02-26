7b7a2c5b5d4fd6f2faf445e429ef58198e48fa1a
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-02-26 14:59:06 +0100

    Password hashing fixed

--- backend/Cookbook/Cookbook.Domain/Services/PasswordHashingService.cs
+++ backend/Cookbook/Cookbook.Domain/Services/PasswordHashingService.cs
@@ -11,25 +11,25 @@ namespace Cookbook.Domain.Services
     public class PasswordHashingService : IPasswordHashingService
     {
         private const int _iterations = 350000;
-        private readonly HashAlgorithmName _algorithmName = HashAlgorithmName.SHA3_512;
+        private readonly HashAlgorithmName _algorithmName = HashAlgorithmName.SHA1;
         private const int _keySize = 64;
 
         public bool IsPasswordValid(string password, string hash, string salt)
         {
-            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, Encoding.UTF8.GetBytes(salt), _iterations, _algorithmName, _keySize);
-            return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(hash));
+            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, Convert.FromBase64String(salt), _iterations, _algorithmName, _keySize);
+            return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromBase64String(hash));
         }
 
         public (string password, string salt) HashPassword(string password)
         {
             var salt = RandomNumberGenerator.GetBytes(32);
-            var iteration = 350000;
-            var hashAlgorithm = HashAlgorithmName.SHA3_512;
-            var keySize = 64;
+            var iteration = _iterations;
+            var hashAlgorithm = _algorithmName;
+            var keySize = _keySize;
 
-            var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, iteration, hashAlgorithm, keySize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, iteration, hashAlgorithm, keySize);
 
-            return (Convert.ToHexString(hash), Convert.ToBase64String(salt));
+            return (Convert.ToBase64String(hash), Convert.ToBase64String(salt));
         }
 
 
