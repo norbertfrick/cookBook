fc4246458c475ab538bdf6ed05c3b77fcf2c5d2e
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-02-26 14:59:31 +0100

    Domain Module added

---
+++ backend/Cookbook/Cookbook.Domain/DomainModule.cs
@@ -0,0 +1,22 @@
+﻿using Cookbook.Domain.Interfaces;
+using Cookbook.Domain.Interfaces.Services;
+using Cookbook.Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Domain
+{
+    public static class DomainModule
+    {
+        public static void AddDomainModule(this IServiceCollection services)
+        {
+            services.AddScoped<ILoginService, LoginService>();
+            //services.AddScoped<IRecipeService, RecipeService>();
+            services.AddScoped<IPasswordHashingService, PasswordHashingService>();
+        }
+    }
+}
