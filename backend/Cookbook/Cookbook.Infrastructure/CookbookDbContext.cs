ad4c799e8b10783749350a35c0da019ff3d6ad9e
Norbert Frick <norbert.frick.external@hexagonsi.com>
2024-02-26 14:58:26 +0100

    DbContext and migrations added

---
+++ backend/Cookbook/Cookbook.Infrastructure/CookbookDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Cookbook.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Infrastructure
+{
+    public class CookbookDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public CookbookDbContext(DbContextOptions<CookbookDbContext> options):base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Cookbook;Trusted_Connection=True;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer();
+            base.OnConfiguring(optionsBuilder);
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
---
+++ backend/Cookbook/Cookbook.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,25 @@
+﻿using Cookbook.Domain.Interfaces.Repositories;
+using Cookbook.Domain.Interfaces.Services;
+using Cookbook.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cookbook.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static void AddInfrastructureModule(this IServiceCollection services, IConfiguration config)
+        {
+            var dbConnectionString = config.GetConnectionString("CookbookDb");
+            services.AddDbContext<CookbookDbContext>(options => options.UseSqlServer(dbConnectionString, x => x.MigrationsAssembly("Cookbook.Infrastructure")));
+
+            //services.AddScoped<IImageService, ImageService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+
+
+        }
+    }
+}
